a50cd8d1f1d9332bec492f53746a1824cde5c416
Mitsutan <petitcom_hondou@icloud.com>
2023-12-18 22:50:35 +0900

    バックエンドのことを考えてformを作った
    これでいいのかは分からない

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,50 +1,61 @@
-﻿export default function App() {
-  return (
-    <>
-        <div className="slideshow">
-			<input type="radio" name="slideshow" id="switch1" defaultChecked />
-			<input type="radio" name="slideshow" id="switch2" />
-			<input type="radio" name="slideshow" id="switch3" />
-			<input type="radio" name="slideshow" id="switch4" />
+﻿"use client"
 
-			<div className="slideContents">
-				<section id="slide1">
-					<p>
-						こんにちは<br />
-						あなたの名前を教えてください
-					</p>
-					<input type="text" name="name" />
-				</section>
-				<section id="slide2">
-					<p>
-						あなたのIDを決めてください<br />
-						世界でひとつの
-					</p>
-					<input type="text" name="id" />
-				</section>
-				<section id="slide3">
-					<p>
-						あなたのメールアドレスを教えてください
-					</p>
-					<input type="email" name="mail" />
-				</section>
-				<section id="slide4">
-					<p>
-						あなたのパスワードを教えてください
-					</p>
-					<input type="password" name="password" />
-				</section>
+export default function App() {
+
+	const handleSubmit = (event: { preventDefault: () => void; }) => {
+		// ここでサーバーに送信する処理を書くとか？
+		event.preventDefault();
+		console.log('Form submitted!');
+		const formData = new FormData(document.getElementById('f') as HTMLFormElement);
+		// (document.getElementById('f') as HTMLFormElement).submit();
+		console.log(Array.from(formData.entries()));
+	};
+	return (
+		<>
+			<div className="slideshow">
+				<input type="radio" name="slideshow" id="switch1" defaultChecked />
+				<input type="radio" name="slideshow" id="switch2" />
+				<input type="radio" name="slideshow" id="switch3" />
+				<input type="radio" name="slideshow" id="switch4" />
+
+				<form className="slideContents" id="f">
+					<section id="slide1">
+						<p>
+							こんにちは<br />
+							あなたの名前を教えてください
+						</p>
+						<input type="text" name="name" />
+					</section>
+					<section id="slide2">
+						<p>
+							あなたのIDを決めてください<br />
+							世界でひとつのIDです。
+						</p>
+						<input type="text" name="id" />
+					</section>
+					<section id="slide3">
+						<p>
+							あなたのメールアドレスを教えてください
+						</p>
+						<input type="email" name="mail" />
+					</section>
+					<section id="slide4">
+						<p>
+							あなたのパスワードを教えてください
+						</p>
+						<input type="password" name="password" />
+					</section>
+				</form>
+
+				<p className="arrow next">
+					<label htmlFor="switch1" onClick={handleSubmit}></label>
+					<label htmlFor="switch2"></label>
+					<label htmlFor="switch3"></label>
+					<label htmlFor="switch4"></label>
+
+				</p>
 			</div>
 
-			<p className="arrow next">
-				<label htmlFor="switch1"></label>
-				<label htmlFor="switch2"></label>
-				<label htmlFor="switch3"></label>
-				<label htmlFor="switch4"></label>
-
-			</p>
-		</div>
-
-    </>
-  );
+		</>
+	);
 }
