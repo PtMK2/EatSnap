fde245ad657a4097febed71b71d4353d410fd613
PtMK2 <2101036@s.asojuku.ac.jp>
2024-01-06 06:00:41 +0900

    口コミ関連完成（仮）
    実際に動かしてみないと分からない

--- backend/model/comment_model.go
+++ backend/model/comment_model.go
@@ -1,6 +1,11 @@
 package model
 
-import "time"
+import(
+    "time"
+    "gorm.io/gorm"
+
+    "github.com/PtMK2/EatSnap/backend/database"
+)
 
 type Comment struct {
     CommentID     int    `json:"comment_id" gorm:"primaryKey"`
@@ -11,4 +16,57 @@ type Comment struct {
     PostTime      time.Time `json:"post_time"`
     // 関連するメディア情報を含める
     Media         []CommentMedia `json:"media" gorm:"foreignKey:CommentID"`
+}
+
+var db = database.DB
+
+func GetCommentsByUserID(userID string) ([]Comment, error) {
+    var comments []Comment
+    result := db.Preload("Media").Where("user_id = ?", userID).Find(&comments)
+    if result.Error != nil {
+        return nil, result.Error
+    }
+    return comments, nil
+}
+
+func GetCommentsByShopID(shopID string) ([]Comment, error) {
+    var comments []Comment
+    result := db.Preload("Media").Where("shop_id = ?", shopID).Find(&comments)
+    if result.Error != nil {
+        return nil, result.Error
+    }
+    return comments, nil
+}
+
+func GetAllComments() ([]Comment, error) {
+    var comments []Comment
+    result := db.Preload("Media").Find(&comments)
+    if result.Error != nil {
+        return nil, result.Error
+    }
+    return comments, nil
+}
+
+func CreateComment(comment *Comment) error {
+    result := db.Create(comment)
+    if result.Error != nil {
+        return result.Error
+    }
+    return nil
+}
+
+func UpdateComment(comment *Comment) error {
+    result := db.Save(comment)
+    if result.Error != nil {
+        return result.Error
+    }
+    return nil
+}
+
+func DeleteCommentByUserID(commentID int) error {
+    result := db.Delete(&Comment{}, commentID)
+    if result.Error != nil {
+        return result.Error
+    }
+    return nil
 }
