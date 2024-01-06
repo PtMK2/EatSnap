993f81de4c8f28d2c020c6ce314e55ac49d338c0
PtMK2 <2101036@s.asojuku.ac.jp>
2024-01-07 03:34:55 +0900

    router.goを作成
    main.goに記述してあるルーター関連をrouter.goに書き直した。

---
+++ backend/router/router.go
@@ -0,0 +1,27 @@
+package router
+
+import (
+	//"github.com/PtMK2/EatSnap/backend/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func GetRouter() *gin.Engine {
+	router := gin.Default()
+
+	//router.LoadHTMLGlob("templates/*.html")
+
+	// こんな感じでルーティングを追加していく
+	//ただフロントがどうなっているかわからないのでコメントアウトしとく
+
+	// router.GET("/signup", controller.GetSignup)
+	// router.POST("/signup", controller.PostSignup)
+	// router.GET("/login", controller.GetLogin)
+	// router.POST("/login", controller.PostLogin)
+	// router.GET("/top", controller.GetTop)
+	// router.GET("/home", controller.GetHome)
+	// router.GET("/logout", controller.GetLogout)
+	// router.GET("/comment/:comment_id", controller.GetComment)
+	// router.POST("/comment", controller.PostComment)
+
+	return router
+}
