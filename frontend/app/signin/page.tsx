7662833638cd881f81232522c7619233330ed415
PtMK2 <2101036@s.asojuku.ac.jp>
2024-01-12 21:28:39 +0900

    バックエンドとの接続部分作成

--- frontend/app/signin/page.tsx
+++ frontend/app/signin/page.tsx
@@ -1,21 +1,44 @@
 "use client"
 import "./signin.css"
+import axios from 'axios';
+import { useState } from 'react';
+
 export default function App() {
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const response = await axios.post('http://localhost:8080/login', {
+            id: id,
+            password: password
+        });
+
+        if (response.data.success) {
+            // ログイン成功
+            console.log("ログイン成功");
+        } else {
+            // ログイン失敗
+            console.log("ログイン失敗");
+        }
+    }
+
     return (   
         <div className="formContainer">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>ログインフォーム</h1>
                 <hr />
                 <div className="uiForm">
                     <div className="formField">
                         <label>ID</label>
-                        <input type="text" placeholder="ID" name="id" />
+                        <input type="text" placeholder="ID" name="id" value={id} onChange={(e) => setId(e.target.value)} />
                     </div>
                     <div className="formField">
                         <label>パスワード</label>
-                        <input type="text" placeholder="パスワード" name="password" />
+                        <input type="password" placeholder="パスワード" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
-                    <button className="submitButton">ログイン</button>
+                    <button className="submitButton" type="submit">ログイン</button>
                 </div>
             </form>
         </div>
