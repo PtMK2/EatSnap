22c9100ae801f799265fd2e688ab396ab5f948e9
PtMK2 <2101036@s.asojuku.ac.jp>
2024-01-12 11:36:25 +0900

    変更

--- backend/controller/mypage_controller.go
+++ backend/controller/mypage_controller.go
@@ -1,21 +1,9 @@
 package controller
 
 import (
-	"net/http"
-	"os"
-
-	"github.com/PtMK2/EatSnap/backend/model"
-	model_redis "github.com/PtMK2/EatSnap/backend/model/redis"
 	"github.com/gin-gonic/gin"
 )
 
 func GetMypage(c *gin.Context) {
-	user := model.User{}
-	cookieKey := os.Getenv("LOGIN_USER_ID_KEY")
-	userId := model_redis.GetSession(c, cookieKey)
-	if userId != nil {
-		user = model.GetOneUser(userId.(string))
-	}
-
-	c.HTML(http.StatusOK, "mypage.html", gin.H{"user": user})
+	renderPage(c, "mypage.html")
 }
