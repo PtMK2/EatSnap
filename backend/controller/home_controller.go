c81e05ae9f863ce8565a11e9e9d59c9f545b580b
PtMK2 <2101036@s.asojuku.ac.jp>
2024-01-12 10:00:43 +0900

    gethome関数を作成

--- backend/controller/home_controller.go
+++ backend/controller/home_controller.go
@@ -3,9 +3,19 @@ package controller
 import (
 	"net/http"
 
+	"github.com/PtMK2/EatSnap/backend/model"
 	"github.com/gin-gonic/gin"
 )
 
-func GetTop(c *gin.Context) {
-	c.HTML(http.StatusOK, "home.html", nil)
+func getHome(c *gin.Context) {
+	user := model.User{}
+	cookieKey := os.Getenv("LOGIN_USER_ID_KEY")
+	userId := model_redis.GetSession(c, cookieKey)
+	if userId != nil {
+		user = model.GetOneUser(userId.(string))
+	}
+
+	c.HTML(http.StatusOK, "home.html", gin.H{
+		"user": user,
+	})
 }
