46c762c2683863cc065df3d230c146489f629e09
2101002ishikawa <2101002@s.asojuku.ac.jp>
2024-01-13 13:41:35 +0900

    セッション変更

--- backend/controller/login_controller.go
+++ backend/controller/login_controller.go
@@ -1,11 +1,10 @@
 package controller
 
 import (
+	"log"
 	"net/http"
-	"os"
 
 	"github.com/PtMK2/EatSnap/backend/model"
-	model_redis "github.com/PtMK2/EatSnap/backend/model/redis"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
@@ -19,22 +18,22 @@ func PostSignup(c *gin.Context) {
 	pw := c.PostForm("password")
 	name := c.PostForm("user_name")
 	mail := c.PostForm("user_mail")
-	user, err := model.Signup(id, pw, name, mail)
+	_, err := model.Signup(id, pw, name, mail)
 	if err != nil {
 		c.Redirect(301, "/signup")
 		return
 	}
-
-	c.HTML(http.StatusOK, "home.html", gin.H{"user": user})
+	// c.HTML(http.StatusOK, "home.html", gin.H{"user": user})
+	PostLogin(c, id, pw)
 }
 
 func GetLogin(c *gin.Context) {
 	c.HTML(http.StatusOK, "login.html", nil)
 }
 
-func PostLogin(c *gin.Context) {
-	id := c.PostForm("user_id")
-	pw := c.PostForm("password")
+func PostLogin(c *gin.Context, id, pw string) {
+	// id := c.PostForm("user_id")
+	// pw := c.PostForm("password")
 	user, err := model.Login(id, pw)
 	if err != nil {
 		c.Redirect(301, "/login")
@@ -47,7 +46,10 @@ func PostLogin(c *gin.Context) {
 }
 
 func GetLogout(c *gin.Context) {
-	cookieKey := os.Getenv("LOGIN_USER_ID_KEY")
-	model_redis.DeleteSession(c, cookieKey)
+	session := sessions.Default(c)
+	log.Println("セッション取得")
+	session.Clear()
+	log.Println("クリア処理")
+	session.Save()
 	c.Redirect(http.StatusFound, "/")
 }
