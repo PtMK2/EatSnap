609fbe2d47828852762433bcad88308f55c3bc47
2101002ishikawa <2101002@s.asojuku.ac.jp>
2024-01-22 17:28:57 +0900

    経過観察

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,6 +6,7 @@ import HomeBar from '../components/HomeBar/Homebar';
 import List from '../components/List/List';
 import Map from '../components/Map/Map';
 import { getPlacesData } from '../api/index';
+import Autocomplete from '@/components/GoogleMap';
 
 export default function Home() {
     type Place = {
@@ -25,7 +26,7 @@ export default function Home() {
     const [filteredPlaces, setFilteredPlaces] = useState<Place[]>([]);
     const [places, setPlaces] = useState<Place[]>([]);
   
-    const [autocomplete, setAutocomplete] = useState(null);
+    const [autocomplete, setAutocomplete] = useState<typeof Autocomplete | null>(null);
     const [childClicked, setChildClicked] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
   
@@ -45,21 +46,21 @@ export default function Home() {
       if (bounds) {
         setIsLoading(true);
   
-  
-        getPlacesData(type, bounds.sw, bounds.ne)
+        //緯度経度を指定して、その範囲内のレストラン情報を取得する　だが、緯度経度データをどこから持ってきているかが分からない
+        getPlacesData(type, bounds?.sw, bounds?.ne)
           .then((data) => {
-            setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
+            setPlaces(data.filter((place: { name: any; num_reviews: number; }) => place.name && place.num_reviews > 0));
             setFilteredPlaces([]);
-            setRating('');
+            setRating(0);
             setIsLoading(false);
           });
       }
     }, [bounds, type]);
   
-    const onLoad = (autoC) => setAutocomplete(autoC);
+    const onLoad = (autoC: React.SetStateAction<Place[]>) => setAutocomplete(autoC);
   
     const onPlaceChanged = () => {
-      const lat = autocomplete.getPlace().geometry.location.lat();
+      const lat = autocomplete?.getPlace()?.geometry?.location?.lat();
       const lng = autocomplete.getPlace().geometry.location.lng();
   
       setCoords({ lat, lng });
@@ -68,7 +69,7 @@ export default function Home() {
     return (
       <>
         <CssBaseline />
-        <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
+        <Headers onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
         <Grid container spacing={3} style={{ width: '100%' }}>
           <Grid item xs={12} md={4}>
             <List
